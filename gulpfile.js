cf1c55eb025de57c6cf5d83e5cb8fae65c6a1263
Jay Varner <jayvarner@gmail.com>
2019-05-06 13:20:27 -0400

    Clean up everything.

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,14 @@ const sourcemaps = require('gulp-sourcemaps');
 const express = require('express');
 
 const PORT = process.env.PORT || '8000';
-const USWDS_DIST = 'node_modules/uswds/dist';
-const USWDS_DIST_DIR = path.join(__dirname, ...USWDS_DIST.split('/'));
 
 // If moving assets to a sub-directory, change '.' to that sub-directory.
 // e.g. 'assets', or 'public/assets'.
 const ASSETS_DIR = path.join(__dirname, '.');
 
-gulp.task('copy-uswds-assets', () => {
-  return gulp.src(`${USWDS_DIST}/@(js|fonts|img)/**/**`)
-  .pipe(gulp.dest(`${ASSETS_DIR}/vendor/uswds`));
-});
-
 gulp.task('sass', () => {
   return gulp.src(`${ASSETS_DIR}/sass/**/*.scss`)
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      includePaths: [
-        path.join(USWDS_DIST_DIR, 'scss'),
-      ]
-    }).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(`${ASSETS_DIR}/css`));
 });
@@ -41,10 +29,9 @@ gulp.task('watch', ['default', 'sass:watch'], done => {
 
   app.use(express.static(__dirname));
   app.listen(PORT, () => {
-    gutil.log(`Development server listening at`,
-              gutil.colors.green(`http://localhost:${PORT}`));
+    gutil.log(`Development server listening at`, gutil.colors.green(`http://localhost:${PORT}`));
     done();
   });
 });
 
-gulp.task('default', ['sass', 'copy-uswds-assets']);
+gulp.task('default', ['sass']);
